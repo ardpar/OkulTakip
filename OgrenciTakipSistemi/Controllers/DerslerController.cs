7c0d9804f9ad18c08d16c69242dedbbad44aa483
Arda Parlak <35842477+ardpar@users.noreply.github.com>
2020-12-06 02:12:28 +0300

    chnage

--- OgrenciTakipSistemi/Controllers/DerslerController.cs
+++ OgrenciTakipSistemi/Controllers/DerslerController.cs
@@ -28,5 +28,30 @@ namespace OgrenciTakipSistemi.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        
+        public IActionResult Sil(int id)
+        {
+            var dersler = context.Dersler.Find(id);
+            context.Remove(dersler);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DersGetir(int id)
+        {
+            var ders = context.Dersler.Find(id);
+
+            return View(ders);
+        }
+        [HttpPost]
+        public IActionResult Guncelle(Ders d)
+        {
+            var ders = context.Dersler.Find(d.Id);
+            ders.Isim = d.Isim;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
